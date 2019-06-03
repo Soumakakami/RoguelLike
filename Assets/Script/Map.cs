6ae36d5c8e1bc6204f0ad944e39131421cf19816
Kakami Souma <nkc.souma.kakami@gmail.com>
2019-06-03 12:41:45 +0900

    no message

---
+++ Assets/Script/Map.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+	[SerializeField] GameObject wall;
+	int  a=1;
+	// Start is called before the first frame update
+	void Start()
+    {
+		test(a);
+		Debug.Log(a);
+    }
+
+	void test(in int _test)
+	{
+		Debug.Log(_test);
+	}
+}
