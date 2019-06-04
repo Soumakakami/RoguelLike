f709e41c3cd070981e098296942143b738bc1ec8
Kakami Souma <nkc.souma.kakami@gmail.com>
2019-06-04 12:05:26 +0900

    ついか

--- Assets/Script/Map.cs
+++ Assets/Script/Map.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class Map : MonoBehaviour
 {
 	[SerializeField] GameObject wall;
-	int  a=1;
+	[SerializeField, Header("マップサイズ")] Vector2Int mapSize;
 	// Start is called before the first frame update
 	void Start()
     {
-		test(a);
-		Debug.Log(a);
+		MapFrame();
     }
 
-	void test(in int _test)
+	void MapFrame()
 	{
-		Debug.Log(_test);
+		for (int i = 0; i < mapSize.x; i++)
+		{
+			for (int j = 0; j < mapSize.y; j++)
+			{
+				if (i==0||j==0)
+				{
+					Instantiate(wall, new Vector3(i, 0, j), Quaternion.identity);
+				}
+
+				else if (i==mapSize.x-1||j==mapSize.y-1)
+				{
+					Instantiate(wall, new Vector3(i, 0, j), Quaternion.identity);
+				}
+			}
+		}
 	}
 }
---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+	public GameObject obj;
+	MeshFilter objFilter;
+	MeshRenderer objRenderer;
+
+	Vector3[] vertices;
+	int[] triangles;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		objFilter=obj.GetComponent<MeshFilter>();
+		objRenderer = obj.GetComponent<MeshRenderer>();
+
+		Mesh mesh = new Mesh();
+
+		vertices = new Vector3[GetComponent<MeshFilter>().mesh.vertices.Length];
+		triangles = new int[GetComponent<MeshFilter>().mesh.triangles.Length];
+
+
+		for (int i = 0; i < GetComponent<MeshFilter>().mesh.vertices.Length; i++)
+		{
+			var vector=Camera.main.WorldToViewportPoint(GetComponent<MeshFilter>().mesh.vertices[i]);
+			vector.z = 0;
+			vertices[i] = Camera.main.ViewportToWorldPoint(vector);
+		}
+
+		for (int i = 0; i < GetComponent<MeshFilter>().mesh.triangles.Length; i++)
+		{
+			triangles[i] = GetComponent<MeshFilter>().mesh.triangles[i];
+		}
+
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+
+
+		mesh.RecalculateNormals();
+		mesh.RecalculateTangents();
+
+		objFilter.mesh = mesh;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		Mesh mesh = new Mesh();
+
+		for (int i = 0; i < GetComponent<MeshFilter>().mesh.vertices.Length; i++)
+		{
+			var vector = Camera.main.WorldToViewportPoint(GetComponent<MeshFilter>().mesh.vertices[i]);
+			vertices[i] = Camera.main.ViewportToWorldPoint(vector);
+		}
+
+		for (int i = 0; i < GetComponent<MeshFilter>().mesh.triangles.Length; i++)
+		{
+			triangles[i] = GetComponent<MeshFilter>().mesh.triangles[i];
+		}
+
+		obj.transform.rotation = transform.rotation;
+
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+
+
+		mesh.RecalculateNormals();
+		mesh.RecalculateTangents();
+
+		objFilter.mesh = mesh;
+	}
+}
