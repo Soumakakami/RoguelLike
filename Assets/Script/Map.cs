7fee71e6d127ff6051a07700c714bbba34435f6a
Kakami Souma <nkc.souma.kakami@gmail.com>
2019-06-05 12:53:38 +0900

    kousinn

--- Assets/Script/Map.cs
+++ Assets/Script/Map.cs
@@ -6,11 +6,24 @@ public class Map : MonoBehaviour
 {
 	[SerializeField] GameObject wall;
 	[SerializeField, Header("マップサイズ")] Vector2Int mapSize;
+	[SerializeField, Header("部屋の最小値")] Vector2Int minRoomSize;
+
+	int[,] mapDate;
+
 	// Start is called before the first frame update
 	void Start()
-    {
+	{
+		mapDate = new int[mapSize.x, mapSize.y];
 		MapFrame();
-    }
+
+	}
+
+	public struct Room
+	{
+		public Vector2Int roomSize;		//部屋の左下の頂点座標
+		public int width;				//横幅
+		public int height;				//縦幅
+	}
 
 	void MapFrame()
 	{
@@ -18,16 +31,23 @@ public class Map : MonoBehaviour
 		{
 			for (int j = 0; j < mapSize.y; j++)
 			{
-				if (i==0||j==0)
+				if (i == 0 || j == 0)
 				{
 					Instantiate(wall, new Vector3(i, 0, j), Quaternion.identity);
+					mapDate[i, j] = 1;
 				}
 
-				else if (i==mapSize.x-1||j==mapSize.y-1)
+				else if (i == mapSize.x - 1 || j == mapSize.y - 1)
 				{
 					Instantiate(wall, new Vector3(i, 0, j), Quaternion.identity);
+					mapDate[i, j] = 1;
 				}
 			}
+		}
+
+		for (int i = 0; i < mapSize.x; i++)
+		{
+
 		}
 	}
 }
